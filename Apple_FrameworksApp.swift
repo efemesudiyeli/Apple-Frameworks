ca426b225e25baabc90fafc6a7cbc856b71075fe
efemesudiyeli <efemesudiyeli@gmail.com>
2024-04-09 01:52:17 +0300

    Init

---
+++ Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple-Frameworks
+//
+//  Created by Efe Mesudiyeli on 9.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
